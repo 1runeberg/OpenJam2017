98e840e24489ac6d23f3f652478b4e9788cad8b5
runeberg <runeberg@outlook.de>
2017-10-07 10:29:50 +0200

    Setup VR stuff

---
+++ Source/OpenJam2017/OpenJam2017.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "OpenJam2017.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, OpenJam2017, "OpenJam2017" );
---
+++ Source/OpenJam2017/OpenJam2017GameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "OpenJam2017GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OPENJAM2017_API AOpenJam2017GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
